632c0bbe36fa3e72a0370bbccf08e261b759d629
Carlos López <c.lopez@kmels.net>
2019-05-28 20:48:12 -0600

    Add Chart.js, Implement download_volumes. Start /yield_curve/<protocol>/<symbol> endpoint

--- __main__.py
+++ __main__.py
@@ -17,17 +17,62 @@ def sense_history():
 def sense_rates():
     mark_event('interest_rates')
 
+def sense_volume():
+    download_volume('supply-volume')
+    download_volume('borrow-volume')
+    download_volume('repayment-volume')
+    download_volume('outstanding-debt')
+
 sched = BackgroundScheduler(daemon=True)
 sched.add_job(sense_rates,'interval',minutes=60)
 sched.add_job(sense_history,'interval',minutes=240)
+sched.add_job(sense_volume,'interval',minutes=1440)
 sched.start()
 
-app = Flask(__name__)
+from flask import render_template, send_from_directory
 
-@app.route("/home")
+app = Flask(__name__, template_folder='html', static_url_path='/static')
+
+from loan_sdk.mongodb import *
+import pymongo
+
+@app.route("/")
 def home():
-    """ Function for test purposes. """
-    return "Welcome Home :) !"
+    rates = db.interest_rates.find({}).sort("snapshotTime",pymongo.DESCENDING)
+    return render_template('curve.html', rates=list(rates))
+
+from datetime import *
+
+@app.route("/yield_curve/<protocol>/<symbol>")
+def yield_curve(protocol, symbol):
+    # curve points
+    # 30d, 2y, 10y, 30y AHEAD
+    # if 30y == 3d => 15m, 6h, 1d, 3d AHEAD
+    timespot_diffs = [timedelta(days=3), timedelta(days=1), timedelta(minutes=15), timedelta(hours=6)]
+    time_now = datetime.utcnow()
+    timespots = [time_now + diff for diff in timespot_diffs]
+    borrow_curve_dots = []
+    supply_curve_dots = []
+    for t in timespots:
+        rates = list(db.interest_rates.find({
+            "snapshotTime": {"$lt": time_now.strftime(date_format)}
+        }).sort("snapshotTime", pymongo.DESCENDING).limit(1))
+
+        print(time_now.strftime(date_format),"Found rates",len(rates))
+        for rate in rates:
+            for interest_rate in rate['interest_rates']:
+                if interest_rate['provider'] == protocol:
+                    for borrow in interest_rate['borrow']:
+                        if borrow['symbol'] == symbol:
+                            borrow_curve_dots.append(borrow['rate'])
+                    for supply in interest_rate['supply']:
+                        if supply['symbol'] == symbol:
+                            supply_curve_dots.append(supply['rate'])
+
+    print("borrow dots",borrow_curve_dots)
+    print("supply dots",supply_curve_dots)
+
+    return render_template('curve.html', rates=[])
 
 if __name__ == "__main__":
     app.run(debug=True)
--- loan_sdk/endpoints.py
+++ loan_sdk/endpoints.py
@@ -1,10 +1,26 @@
 from .mongodb import db as DB
 from .settings import *
 
+date_format = "%Y-%m-%dT%H:%M:%SZ"
+
 db = {
     'agreements': DB.agreements,
     'interest_rates': DB.interest_rates,
-    'issuances': DB.issuances
+    'issuances': DB.issuances,
+    'supply-volume': DB.supply_volume,
+    'borrow-volume': DB.borrow_volume,
+    'repayment-volume': DB.repayment_volume,
+    'outstanding-debt': DB.outstanding_debt,
+    'collateral-ratio': DB.collateral_ratio,
+    'top-borrowed-assets': DB.top_borrowed_assets,
+    'top-supplied-assets': DB.top_supplied_assets,
+    'top-outstanding-debt-assets': DB.outstanding_debt_assets,
+    'top-repaid-assets': DB.top_repaid_assets,
+    'top-borrowed-protocols': DB.top_borrowed_protocols,
+    'top-supplied-protocols': DB.top_supplied_protocols,
+    'top-repaid-protocols': DB.top_repaid_protocols,
+    'metadata': DB.debt_issuance_metadata,
+    'dipor': DB.dipor
 }
 headers = {
     'x-api-key': API_KEY
@@ -12,12 +28,16 @@ headers = {
 parameters = {
     'agreements': { 'limit': 100 },
     'interest_rates': {},
-    'issuances': { 'limit': 100 }
+    'issuances': { 'limit': 100 },
+    'supply-volume': {'intervalType': "Day"},
+    'borrow-volume': {'intervalType': "Day"}
 }
+protocols = ['Dharma','MakerDao','Compound','CompoundV2']
 urls = {
     'agreements': "https://api.loanscan.io/v1/agreements",
     'interest_rates': "https://api.loanscan.io/v1/interest-rates",
-    'issuances': "https://api.loanscan.io/v1/issuances"
+    'issuances': "https://api.loanscan.io/v1/issuances",
+    'supply-volume': 'https://api.loanscan.io/v1/stats/supply-volume'
 }
 
 import requests
@@ -27,7 +47,7 @@ from datetime import datetime
 def get_response(endpoint, page = 1) -> dict:
     limit = parameters[endpoint].get('limit')
 
-    if page:
+    if page and limit:
         offset = (page-1)*limit
         parameters[endpoint]['offset'] = offset
     url = urls[endpoint]
@@ -39,15 +59,18 @@ def get_response(endpoint, page = 1) -> dict:
         params = parameters[endpoint]
     )
     text = response.content
+    if len(text) == 0:
+        return {}
     print(len(text), " bytes")
     payload = json.loads(text)
-    if page:
+
+    if page and limit:
         del parameters[endpoint]['offset']
     return payload
 
 def mark_event(endpoint):
     snapshot = {
-        'snapshotTime': datetime.utcnow().strftime("%Y-%m-%DT%H:%M:%SZ")
+        'snapshotTime': datetime.utcnow().strftime(date_format)
     }
     snapshot[endpoint] = get_response(endpoint, page = False)
     print("Inserting one ", endpoint, " ...")
@@ -72,6 +95,28 @@ def scan_history(endpoint, page_n, stop_criteria):
     if not end:
         scan_history(endpoint, page_n + 1, stop_criteria)
 
+def scan_stats(endpoint, page_n, stop_criteria):
+    print("Scanning stats ... ")
+    page = get_response(endpoint, page_n)
+    page_items = page
+
+    end = False
+    print("page items: ", len(page_items))
+    for item in page_items:
+        if not item:
+            continue
+        if stop_criteria(item):
+            print("Stopping @ ", item['date'])
+            end = True
+            return
+        else:
+            print("Saving ", item['date'])
+            item['protocol'] = parameters[endpoint]['protocol']
+            db[endpoint].insert_one(item)
+    end = True
+    #if not end:
+    #    scan_stats(endpoint, page_n + 1, stop_criteria)
+
 import pymongo
 def download_issuances():
     """Returns sorted by date"""
@@ -94,3 +139,16 @@ def download_agreements():
     print("Stop at ", stop_at)
     scan_history(endpoint, page_n = 1,
         stop_criteria = lambda it: it['creationTime'] <= stop_at)
+
+def download_volume(endpoint):
+    stop_at = "1970-01-01T00:00:00Z" # first record ever
+    _newest = list(db[endpoint].find({}).sort("date", pymongo.DESCENDING).limit(1))
+    print("Download volume ", endpoint, "...",len(_newest))
+    for i in _newest:
+        stop_at = i['date']
+    for protocol in protocols:
+        print(protocol," Stop at ", stop_at)
+        parameters[endpoint]['protocol'] = protocol
+        scan_stats(endpoint, page_n = 1,
+            stop_criteria = lambda it: it['date'] <= stop_at)
+        del parameters[endpoint]['protocol']
